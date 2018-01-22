bb2103701b9be9cfa985e3f99d7745cd652e173a
Andray <patriceomoghene@gmail.com>
2018-01-22 17:36:14 +0000

    HTML exercise, with too much data

---
+++ vagrant/w2d1/client/htmlchunks.js
@@ -0,0 +1,17 @@
+var https = require('https')
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    var buffer = "";
+    response.on('data', function(data) {
+      buffer += data
+      console.log(buffer);
+    });
+    // response.on('end', function() {
+    //   console.log("Done!");
+    // });
+  });
