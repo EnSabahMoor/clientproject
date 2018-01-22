6c806338bd0e58cc7a3a2197ac952fbd89974db1
Andray <patriceomoghene@gmail.com>
2018-01-22 21:46:04 +0000

    Corrected module

---
+++ vagrant/w2d1/client/callbacks/http-functions.js
@@ -0,0 +1,21 @@
+module.exports = function getHTML (options, callback) {
+      var https = require('https');
+    https.get(options, function(response) {
+     response.setEncoding('utf8');
+     var html = "";
+     response.on('data', function(data) {
+       html += data;
+     });
+     response.on('end', function() {
+       callback(html);
+     });
+   });
+  // function printHTML (html) {
+  //   console.log(html);
+  // }
+  //
+  // var requestOptions = {
+  //   host: 'sytantris.github.io',
+  //   path: '/http-examples/step4.html'
+  // };
+};
